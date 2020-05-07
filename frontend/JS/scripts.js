47b9507bc378d7df8fb8408fe41cc6572077cab3
Seb-Schampaert <64017041+Seb-Schampaert@users.noreply.github.com>
2020-05-07 03:46:33 +0200

    Add files via upload

--- frontend/JS/scripts.js
+++ frontend/JS/scripts.js
@@ -1,25 +1,74 @@
+function randomStyle(){
+	let num = Math.ceil(Math.random()*3);
+	let string = "url(\"img/cube" + num + ".png\")";
+	document.getElementsByTagName("html")[0].style.backgroundImage = string;
+}
+
 
 function xhrReqJson(url, fct, id){
 // passe la réponse de la requête ajax demandant la ressource url
 // à la fonction fct pour fabriquer le contenu de id
 	var xhr = new XMLHttpRequest(); // instancier XMLHttpRequest
+
 	xhr.open('get', url, true); // préparer
+
 	xhr.onload = // callback : fonction anonyme
-		function(){ console.log(xhr.responseText + xhr.responseText.length);
-			fct( JSON.parse(xhr.responseText), id); 
-		}
-	xhr.send() // envoyer
+		function(){ fct( JSON.parse(xhr.responseText), id);		 
+	}
+	xhr.send(); // envoyer
 }
 
 function getProduits(){
-	xhrReqJson(getProduits, makeSelect, ingredients);
+	xhrReqJson("getProduits", makeSelect, "ingredients");
 }
 
-function makeSelect(lng, id){ // faire une liste déroulante
-	var liste = '<select onChange="setLang(this);">';
-	for(var i in lng){
-		liste += '<option value=' + lng[i].id + '>' + lng[i].lib + '</option>';
+
+function setElem(id, content){
+	document.getElementById(id).innerHTML += content;
+}
+
+function makeSelect(prod, id){ // faire une liste déroulante
+	var liste = '<select>';
+	for(var i in prod){
+		liste += '<option value=' + prod[i].id + '>' + prod[i].lib + '</option>';
 	}
 	liste += '</select>';
-	setElem(id, liste);
+	document.getElementById(id).innerHTML += liste;
+}
+
+function ajouterDansFrigo(bouton){
+	let form = bouton.form;
+	if (form.quant.value != false){
+		let url = "localhost/ajouterDansFrigo?ingredient=" + form.ingredients.value + "&quantite=" + form.quant.value;
+		console.log(url);
+		let xhr = new XMLHttpRequest(); // instancier XMLHttpRequest
+		// call proc_ajouterDansFrigo('oeuf', 3, '01-01-2020') ((SQL))
+		// localhost/ajouterDansFrigo?ingredient=oeuf&quantite=3&dateAjout=01-01-2020 ((Browser)) construit en JS
+		//let url = "ajouterDansFrigo?" + "quantite=" + formulaire.quant.value + 
+		xhr.open('get', url, true); // préparer
+		xhr.onload = confirmerEnregistrement;
+		xhr.send() // envoyer
+	}
+}
+
+function confirmerEnregistrement(){
+	console.log( JSON.parse(this.responseText));
+}
+
+
+function userVerification(bouton){
+	let form = bouton.form;
+	let username = form.username.value;
+	let password = form.pswd.value;
+
+	if (username != 0 & password != 0){
+		let url = "localhost/verifierUser?username=" + username + "&password=" + password;
+		let xhr = new XMLHttpRequest();
+
+		xhr.open('get', url, true);
+		xhr.send();
+
+		console.log(xhr.responseText);
+	}
+
 }
