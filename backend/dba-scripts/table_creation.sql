894cc887e9a82ee8b141bcf86c4a451e54cfdd55
Seb-Schampaert <64017041+Seb-Schampaert@users.noreply.github.com>
2020-05-07 03:52:34 +0200

    Add files via upload

--- backend/dba-scripts/table_creation.sql
+++ backend/dba-scripts/table_creation.sql
@@ -1,7 +1,7 @@
-CREATE TABLE tbProduits(
-prodID varchar(3) NOTNULL,
-prodLib varchar(30) NOT NULL,
-CONSTRAINT pkProd PRIMARY KEY(prodID),
+CREATE TABLE tbUnites(
+unitID varchar(3) NOT NULL,
+unitLib varchar (8),
+CONSTRAINT pkUnit PRIMARY KEY(unitID),
 );
 
 CREATE TABLE tbUsers(
@@ -17,19 +17,31 @@ rctLib varchar(30),
 CONSTRAINT pkRct PRIMARY KEY(rctID),
 );
 
-CREATE TABLE tbFrigos(
-frigID	varchar(3) NOT NULL,
-prodID	varchar(3) NOT NULL,
-quantite varchar(3) NOT NULL,
-CONSTRAINT pkFrig PRIMARY KEY(frigID),
-CONSTRAINT fkFrig FOREIGN KEY(prodID) REFERENCES tbProduits(prodID) on delete restrict,
+CREATE TABLE tbProduits(
+prodID varchar(3) NOT NULL,
+prodLib varchar(30) NOT NULL,
+unitID varchar(3) NOT NULL,
+CONSTRAINT pkProd PRIMARY KEY(prodID),
+CONSTRAINT fkProdUnit FOREIGN KEY(unitID) REFERENCES tbUnites(unitID) on delete restrict,
 );
 
-CREATE TABLE tbRecettesEtProduits(
-recID varchar(3) NOT NULL,
+CREATE TABLE tbRecettesProduits(
+rctID varchar(3) NOT NULL,
 prodID	varchar(3) NOT NULL,
-CONSTRAINT pkRecProd PRIMARY KEY(prodId,recID),
-CONSTRAINT fkRecProd FOREIGN KEY(rctID) REFERENCES tbRecettes(rctID) on delete restrict,
-CONSTRAINT fkRecProds FOREIGN KEY(prodID) REFERENCES tbProduits(prodID) on delete restrict,
+CONSTRAINT pkRecProd PRIMARY KEY(prodId,rctID),
+CONSTRAINT fkRecProdRec FOREIGN KEY(rctID) REFERENCES tbRecettes(rctID) on delete restrict,
+CONSTRAINT fkRecProdProd FOREIGN KEY(prodID) REFERENCES tbProduits(prodID) on delete restrict,
 );
+
+CREATE TABLE tbFrigo(
+usrID	varchar(3) NOT NULL,
+prodID	varchar(3) NOT NULL,
+quantite varchar(3) NOT NULL,
+CONSTRAINT pkUsrProd PRIMARY KEY(usrID, prodID),
+CONSTRAINT fkFrigoProd FOREIGN KEY(prodID) REFERENCES tbProduits(prodID) on delete restrict,
+CONSTRAINT fkFrigoUsr FOREIGN KEY(usrID) REFERENCES tbUsers(usrID) on delete restrict,
+);
+
+
+------------------
 
