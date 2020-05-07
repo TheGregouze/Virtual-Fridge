f4c35869d7b6a125b3f95e0947815669fdac0fd3
Seb-Schampaert <64017041+Seb-Schampaert@users.noreply.github.com>
2020-05-07 03:52:13 +0200

    Add files via upload

--- backend/webservices/services-frigo.sql
+++ backend/webservices/services-frigo.sql
@@ -13,3 +13,32 @@ CREATE SERVICE "getProduits"
     URL ON
     METHODS 'GET'
 AS call dba.getProduits();
+
+
+ALTER PROCEDURE "DBA"."verifierUser"(@name varchar(30), @key varchar(20))
+--Sebaztyan Schampaert
+BEGIN
+declare @msg varchar(30);
+set @msg = 'smth';
+call sa_set_http_header('Content-Type','text/html');
+call sa_set_http_header('Access-Control-Allow-Origin', '*'); 
+--creation de procedure et des headers appropries
+
+--utilisation du exists pour verifier si le nom utilisateur est deja employe
+    IF EXISTS (SELECT usrName FROM tbUsers WHERE usrName = name)
+        THEN SELECT (@msg);
+    ELSE
+      BEGIN
+        INSERT INTO DBA.tbUsers (usrID, usrName, usrKey)
+         VALUES (DEFAULT, @name, @key);
+        SELECT ('wew gj');
+      END
+    ENDIF;
+END
+
+CREATE SERVICE "verifierUser" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" METHODS 'GET' AS call DBA.verifierUser(:name, :key);
+
+
+
+
+
