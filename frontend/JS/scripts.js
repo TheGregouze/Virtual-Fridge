177012f2fe700e1de8148661bcb8ac7bdfcadc7c
Seb-Schampaert <64017041+Seb-Schampaert@users.noreply.github.com>
2020-05-09 23:38:58 +0200

    Add files via upload

--- frontend/JS/scripts.js
+++ frontend/JS/scripts.js
@@ -1,23 +1,28 @@
 'use strict'
-let userID;
-let prodList;
+let userID; //sert a verifier si l'utilisateur est connecté, et permet de stocker les produits dans le "frigo" correct
+let prodList; //liste des denrees reconnues par le systeme
+let userList; //stock de manière globale les ID, pseudos et mots de passes des users
+
+
+
 function getProduits(){
-// passe la réponse de la requête ajax demandant la ressource url
-// à la fonction fct pour fabriquer le contenu de id
 	var xhr = new XMLHttpRequest(); // instancier XMLHttpRequest
 
 	xhr.open('get', "getProduits", true); // préparer
-
 	xhr.onload = // callback : fonction anonyme
 		function(){ makeSelect( JSON.parse(xhr.responseText), "produits");		 
 	}
 	xhr.send(); // envoyer
 }
 
-function randomStyle(){
-	let num = Math.ceil(Math.random()*3);
-	let string = "url(\"img/cube" + num + ".png\")";
-	document.getElementsByTagName("html")[0].style.backgroundImage = string;
+function makeSelect(prod, id){ // faire une liste déroulante des produits
+	prodList = prod; //permet d'acceder globalement a la liste 
+	let liste = '<select>';
+	for(let i in prod){
+		liste += '<option value=' + prod[i].id + '>' + prod[i].lib + '</option>';
+	}
+	liste += '</select>';
+	document.getElementById(id).innerHTML += liste;
 }
 
 
@@ -26,13 +31,30 @@ window.onload = function(){
 	let loginChange = document.getElementById("loginRegister");
 	let userSub = document.getElementById("userIDSubmit");
 	let prodSub = document.getElementById("prodSubmit");
+	let suprLigne = document.getElementById("suppressionLigne");
+	let reset = document.getElementById("reset");
 
 	loginChange.addEventListener('click', changeLogin);
-	userSub.addEventListener('click', userSubHandler)
-	prodSub.addEventListener('click', ajouterFrigo)
+	userSub.addEventListener('click', userSubHandler);
+	prodSub.addEventListener('click', ajouterFrigo);
+	suprLigne.addEventListener('click', suppressionLigne);
+	reset.addEventListener('click', resetTable);
 	
 	getProduits();
 }
+
+function suppressionLigne(){
+	document.getElementById("frigo").deleteRow(-1);
+}
+function resetTable(){
+	document.getElementById("frigo").innerHTML = "<tr><th>Produit</th><th>Quantité</th></tr>";
+}
+function randomStyle(){
+	let num = Math.ceil(Math.random()*3);
+	let string = "url(\"img/cube" + num + ".png\")";
+	document.getElementsByTagName("html")[0].style.backgroundImage = string;
+}
+
 
 //permet d'avoir une distinction entre une tentative de création de compte et d'une connexion a un compte
 function changeLogin(){
@@ -49,12 +71,12 @@ function userSubHandler(){
 
 	let form = document.getElementById("userID");
 
-
 	if(form.username.value && form.pswd.value){//si les deux champs ne sont pas remplis, rien ne se passe du coté JS, la page affiche que les champs sont requis
+		userListRequest();
+		console.log('1');
 		if (form.sub.value == "Login"){
 			console.log("test login");
-			userListRequest(form.username.value, form.pswd.value);
-
+			userLogin(form.username.value, form.pswd.value);
 		}else if(form.sub.value == "Register"){
 			console.log("test Register");
 			userRegister(form.username.value, form.pswd.value);
@@ -62,41 +84,39 @@ function userSubHandler(){
 	}
 }
 
-function userListRequest(name, password){
+function userListRequest(){ //recuperation de la liste des IDS , psuedos et mot de passes
 	let xhr = new XMLHttpRequest();
-	let userList;
-
 	xhr.open('get','listUsers',true);
 	xhr.onload = function(){
 		userList = JSON.parse(xhr.responseText); 
 		console.log(userList)
-		userLogin(userList, name, password)};
+		};
 	xhr.send();
 }
 
-function userLogin(userList, name, password){
+function userIndex(name){//retourne -1 si l'utilisateur n'existe pas, et le position dans le tableau userList si oui
 	let i = 0;
-	let userIndex;
-	let existant;
-
+	let userIndex = -1; 
 	for(i in userList){
 		if(userList[i].username == name){
-			existant = true;
 			userIndex = i;
 		}
 	}
+	return userIndex; 
+}
 
-	if (existant){
-		if(userList[userIndex].pswd == password){
-			//loginnn
+function userLogin(name, password){
+	let uIndex = userIndex(name);
+
+	if (uIndex >= 0){
+		if(userList[uIndex].pswd == password){//test si le mot de passe est valide	
 			console.log("Connection!");
 			getUserID(name);
-
 		}else{
 			console.log("Mauvais mot de passe");
 		}
 	}else{
-		console.log("compte introuvable")
+		console.log("compte introuvable");
 	}
 }
 
@@ -108,15 +128,23 @@ function getUserID(name){
 	xhr.send();
 }
 
+function userRegister(name, password){
+	let uIndex = userIndex(name);
 
-function makeSelect(prod, id){ // faire une liste déroulante
-	prodList = prod;
-	var liste = '<select>';
-	for(var i in prod){
-		liste += '<option value=' + prod[i].id + '>' + prod[i].lib + '</option>';
+	if (uIndex == -1){//si le nom est libre
+		console.log('nouv ercreat');
+		let xhr = new XMLHttpRequest();
+		let url = 'register?username=' + name + '&password=' + password;
+
+		xhr.open('get', url, true);
+		xhr.onload = function(){
+			userID = xhr.responseText;
+		}
+		xhr.send();
+	}else{
+		console.log('not available');
 	}
-	liste += '</select>';
-	document.getElementById(id).innerHTML += liste;
+	userListRequest();
 }
 
 
