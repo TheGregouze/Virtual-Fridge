e2a402a8d6f95fec80d8efe5a92af2eed5bc990c
LiamBeaufils <64018717+LiamBeaufils@users.noreply.github.com>
2020-05-10 17:35:42 +0200

    Create fonctions.sql

---
+++ backend/scripts-api-rest/fonctions.sql
@@ -0,0 +1,11 @@
+ALTER FUNCTION "DBA"."testFrigo"(@produits int, @userID int)
+RETURNS varchar(7)--retourne vrai si il y a deja une instance
+BEGIN
+    IF EXISTS (SELECT usrID FROM tbFrigo WHERE prodID = @produits AND usrID = @userID)
+       THEN return ('true');
+    ELSE
+       return ('false');
+    endif;
+END
+
+
