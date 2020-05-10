415c65beeb7dff189f2a561ab884cc3354b9c1e0
LiamBeaufils <64018717+LiamBeaufils@users.noreply.github.com>
2020-05-11 00:10:12 +0200

    Create recupererUnite.sql

---
+++ backend/scripts-api-rest/recupererUnite.sql
@@ -0,0 +1,15 @@
+ALTER PROCEDURE "DBA"."recupererUnite"()
+RESULT (unitLib VARCHAR(10), unitID int)
+BEGIN
+    call sa_set_http_header('Content-Type','application:json; charset=utf-8');
+    SELECT unitLib, unitID
+    FROM tbUnites
+END
+
+CREATE SERVICE "recupererUnite"
+TYPE 'JSON'
+AUTHORIZATION OFF 
+    USER "DBA"
+    URL ON
+    METHODS 'GET'
+AS call DBA.recupererUnite()
