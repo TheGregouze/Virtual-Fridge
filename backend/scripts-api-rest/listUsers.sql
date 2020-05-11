68300fa02e06832baaabfcada86d80dd4ec8536c
Seb-Schampaert <64017041+Seb-Schampaert@users.noreply.github.com>
2020-05-11 15:57:44 +0200

    Update listUsers.sql

--- backend/scripts-api-rest/listUsers.sql
+++ backend/scripts-api-rest/listUsers.sql
@@ -2,6 +2,7 @@ CREATE PROCEDURE "DBA"."listUsers"()
 RESULT (username char(30), pswd char(30))
 BEGIN
     call sa_set_http_header('Content-Type','application:json; charset=utf-8');
+    call sa_set_http_header('Access-Control-Allow-Origin', '*');
     select usrName, usrKey
     from dba.tbUsers
 END
