c02b6189725dee461c33bdfa67e280037bab3d0e
Seb-Schampaert <64017041+Seb-Schampaert@users.noreply.github.com>
2020-05-11 15:58:00 +0200

    Update recupererUnite.sql

--- backend/scripts-api-rest/recupererUnite.sql
+++ backend/scripts-api-rest/recupererUnite.sql
@@ -2,6 +2,7 @@ CREATE PROCEDURE "DBA"."recupererUnite"()
 RESULT (unitLib VARCHAR(10), unitID int)
 BEGIN
     call sa_set_http_header('Content-Type','application:json; charset=utf-8');
+    call sa_set_http_header('Access-Control-Allow-Origin', '*');
     SELECT unitLib, unitID
     FROM tbUnites
 END
