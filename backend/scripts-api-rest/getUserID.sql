84e69777ffda5dd2e12bd2006538c9a31913406e
Seb-Schampaert <64017041+Seb-Schampaert@users.noreply.github.com>
2020-05-11 15:59:30 +0200

    Update getUserID.sql

--- backend/scripts-api-rest/getUserID.sql
+++ backend/scripts-api-rest/getUserID.sql
@@ -2,6 +2,7 @@ CREATE PROCEDURE "DBA"."getUserID"(@username varchar(30))
 RESULT (userID int)
 BEGIN
     call sa_set_http_header( 'Content-Type', 'text/html' );
+     call sa_set_http_header('Access-Control-Allow-Origin', '*');
     select usrID
     FROM tbUsers
     WHERE usrName = @username;
