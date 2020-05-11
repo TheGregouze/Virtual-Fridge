abfbe6aab3358dc6fad198bd226321d9971e7d20
Seb-Schampaert <64017041+Seb-Schampaert@users.noreply.github.com>
2020-05-11 15:58:31 +0200

    Update Liam Beaufils-recupererRecette.sql

--- backend/scripts-api-rest/Liam Beaufils-recupererRecette.sql
+++ backend/scripts-api-rest/Liam Beaufils-recupererRecette.sql
@@ -2,6 +2,7 @@ CREATE PROCEDURE "DBA"."recupererRecette" ()
 RESULT (rctLib varchar(20),prodLib varchar(20),prodQuant int)
 BEGIN
     call sa_set_http_header('Content-Type','application:json; charset=utf-8');
+     call sa_set_http_header('Access-Control-Allow-Origin', '*');
     SELECT rctLib, prodLib, prodQuant
     FROM tbRecettesProduits
     NATURAL JOIN tbRecettes
