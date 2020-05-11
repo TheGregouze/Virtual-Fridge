e79167a4f6d9f667a79cc18d3f9431671d3c732e
Seb-Schampaert <64017041+Seb-Schampaert@users.noreply.github.com>
2020-05-11 16:00:02 +0200

    Update recupererFrigo.sql

--- backend/scripts-api-rest/recupererFrigo.sql
+++ backend/scripts-api-rest/recupererFrigo.sql
@@ -2,6 +2,7 @@ CREATE PROCEDURE "DBA"."recupererFrigo"(@userID int)
 RESULT (lib varchar(16), quant int, libUnit varchar(10))
 BEGIN
     call sa_set_http_header('Content-Type','application:json; charset=utf-8');
+     call sa_set_http_header('Access-Control-Allow-Origin', '*');
     SELECT prodLib, prodQuant, unitLib
     FROM tbFrigo
     NATURAL JOIN tbProduits
