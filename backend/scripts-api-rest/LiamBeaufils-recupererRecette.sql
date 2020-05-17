fbb30018e7726505acd84b1a8fd2bbdfa7e74f70
Seb-Schampaert <64017041+Seb-Schampaert@users.noreply.github.com>
2020-05-18 01:12:10 +0200

    Update LiamBeaufils-recupererRecette.sql

--- backend/scripts-api-rest/LiamBeaufils-recupererRecette.sql
+++ backend/scripts-api-rest/LiamBeaufils-recupererRecette.sql
@@ -1,9 +1,8 @@
 CREATE PROCEDURE "DBA"."recupererRecette" ()
-RESULT (rctLib varchar(20),prodLib varchar(20),prodQuant int)
+RESULT (recetteID int, rctLib varchar(20),prodLib varchar(20),prodQuant int)
 BEGIN
     call sa_set_http_header('Content-Type','application:json; charset=utf-8');
-     call sa_set_http_header('Access-Control-Allow-Origin', '*');
-    SELECT rctLib, prodLib, prodQuant
+    SELECT tbRecettes.rctID, rctLib, prodLib, prodQuant
     FROM tbRecettesProduits
     JOIN tbRecettes on tbRecettesProduits.rctID = tbRecettes.rctID
     JOIN tbProduits on tbRecettesProduits.prodID = tbProduits.prodID
