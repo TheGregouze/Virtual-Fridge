b499671b64ea2be33f2e1e0bb294b90a8569ca5c
LiamBeaufils <64018717+LiamBeaufils@users.noreply.github.com>
2020-05-18 11:22:22 +0200

    Update LiamBeaufils-Scripts.js

--- frontend/JS/LiamBeaufils-Scripts.js
+++ frontend/JS/LiamBeaufils-Scripts.js
@@ -4,3 +4,19 @@ function recupererRecettes(){
     xhr.onload = analyseRecettes;
     xhr.send();
 }
+-----------------
+	
+	
+function recupProduit() {
+	let xhr = new XMLHttpRequest();
+
+	xhr.open('get', "NombreProduit", true);
+	xhr.onload = traitement;
+	xhr.send();
+}
+
+function traitement() {
+	let reponse = JSON.parse(this.response)
+	let nombre = reponse[0].nbr;
+	document.getElementById('nombreP').innerHTML = "un des "+ nombre + " produits";
+}
