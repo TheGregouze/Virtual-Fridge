433ef0a5d38135e65f8c8a8d0ac3a8e093ade24c
LiamBeaufils <64018717+LiamBeaufils@users.noreply.github.com>
2020-05-18 11:26:24 +0200

    Create LiamBeaufils-NombreProduit.sql

---
+++ backend/scripts-api-rest/LiamBeaufils-NombreProduit.sql
@@ -0,0 +1,18 @@
+CREATE PROCEDURE "DBA"."NombreProduit"()
+
+BEGIN
+	call sa_set_http_header('Content-Type','application:json; charset=utf-8');
+    
+    SELECT count(*) as nbr
+    FROM tbProduits
+    
+END
+
+
+CREATE SERVICE "NombreProduit"
+TYPE 'JSON'
+AUTHORIZATION OFF 
+    USER "DBA"
+    URL ON
+    METHODS 'GET'
+AS call DBA.recupererRecette()
